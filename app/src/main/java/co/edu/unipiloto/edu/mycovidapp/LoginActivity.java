4b158fe7a5f6042df983aaf48aaf5b8fe5bcb7bd
Joahan Sebatian Silva <62309125+S3B4S10@users.noreply.github.com>
2021-01-18 16:21:19 -0500

    Login google funcionando con Login facebook

--- app/src/main/java/co/edu/unipiloto/edu/mycovidapp/LoginActivity.java
+++ app/src/main/java/co/edu/unipiloto/edu/mycovidapp/LoginActivity.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.facebook.login.widget.LoginButton;
 import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.auth.IdpResponse;
 import com.google.android.gms.common.SignInButton;
@@ -16,29 +17,40 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class LoginActivity extends AppCompatActivity {
 SignInButton btn_login;
+LoginButton btn_loginFa;
+//List<AuthUI.IdpConfig> providers = Arrays.asList(new AuthUI.IdpConfig.FacebookBuilder().build(),new AuthUI.IdpConfig.GoogleBuilder().build());
 private static final int RC_SIGN_IN=1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
+        btn_loginFa=(LoginButton)findViewById(R.id.btn_loginfa);
         btn_login= (SignInButton) findViewById(R.id.btn_login);
+
+        btn_loginFa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                signIn(new AuthUI.IdpConfig.FacebookBuilder().build());
+            }
+        });
 
         btn_login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-            signIn();
+                signIn(new AuthUI.IdpConfig.GoogleBuilder().build());
             }
         });
     }
-    private void signIn(){
-        List<AuthUI.IdpConfig> providers = Arrays.asList(new AuthUI.IdpConfig.GoogleBuilder().build());
-        Intent signInIntent=AuthUI.getInstance().createSignInIntentBuilder().setAvailableProviders(providers).build();
+    private void signIn(AuthUI.IdpConfig provedor){
+
+        Intent signInIntent=AuthUI.getInstance().createSignInIntentBuilder().setAvailableProviders(Collections.singletonList(provedor)).build();
         startActivityForResult(signInIntent, RC_SIGN_IN);
+
     }
 
     @Override
